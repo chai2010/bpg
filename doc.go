c86f12f901c62b63280173309c6167db8273a428
chai2010 <chaishushan@gmail.com>
2015-01-12 16:14:22 +0800

    pkgdoc: add example

--- doc.go
+++ doc.go
@@ -22,7 +22,49 @@ Examples
 
 This is a simple example:
 
-	TODO
+	package main
+
+	import (
+		"bytes"
+		"fmt"
+		"image/png"
+		"io/ioutil"
+		"log"
+
+		"github.com/chai2010/bpg"
+	)
+
+	func main() {
+		var buf bytes.Buffer
+		var data []byte
+		var err error
+
+		data, err = ioutil.ReadFile("./testdata/lena512color.bpg")
+		if err != nil {
+			log.Println(err)
+		}
+		info, err := bpg.DecodeInfo(data)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Printf("info: %v\n", info)
+
+		// Decode bpg
+		m, err := bpg.Decode(bytes.NewReader(data))
+		if err != nil {
+			log.Println(err)
+		}
+
+		// save as png
+		if err = png.Encode(&buf, m); err != nil {
+			log.Println(err)
+		}
+		if err = ioutil.WriteFile("output.png", buf.Bytes(), 0666); err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println("Save as output.png")
+	}
 
 
 BUGS
